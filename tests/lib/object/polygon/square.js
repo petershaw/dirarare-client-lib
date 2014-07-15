896b5afc20ac42754c785fcfb2a99ecc31112c28
Kris <kris@opentabs.de>
2014-07-16 00:34:05 +0200

    Polygons will not be zeroed anymore
    Zero a polygon was a helping head for constricting the algorithms. In the real world, we do not want to zero the elements.

--- tests/lib/object/polygon/square.js
+++ tests/lib/object/polygon/square.js
@@ -204,7 +204,7 @@ describe('Simple Square-Polygon', function(){
 		});
 	});
 	
-	describe('in initial moved position should be nulled', function(){
+	describe('in initial moved position should be placed', function(){
 		describe('with disabledLine and disabledFill mode', function(){
 			var s =  new polygon([2,2],[7,2],[7,6],[2,6]);
 			/*
@@ -235,10 +235,11 @@ describe('Simple Square-Polygon', function(){
 
 			it('should return 0,0; 5,0; 5,4 and 0,4', function(done){
 				s.render(function(data){
-					data.should.containEql([0,0]);
-					data.should.containEql([5,0]);
-					data.should.containEql([5,4]);
-					data.should.containEql([0,4]);
+					data.should.containEql([2,2]);
+					data.should.containEql([7,2]);
+					data.should.containEql([7,6]);
+					data.should.containEql([2,6]);
+
 					done();
 				});
 				
@@ -276,32 +277,32 @@ describe('Simple Square-Polygon', function(){
 
 				s.render(function(data){		
 					// Points
-					data.should.containEql([0,0]);
-					data.should.containEql([5,0]);
-					data.should.containEql([5,4]);
-					data.should.containEql([0,4]);
+					data.should.containEql([2,2]);
+					data.should.containEql([7,2]);
+					data.should.containEql([7,6]);
+					data.should.containEql([2,6]);
 					
 					// bottom Line
-					data.should.containEql([1,0]);
-					data.should.containEql([2,0]);
-					data.should.containEql([3,0]);
-					data.should.containEql([4,0]);
+					data.should.containEql([3,2]);
+					data.should.containEql([4,2]);
+					data.should.containEql([5,2]);
+					data.should.containEql([6,2]);
 					
 					// right up
-					data.should.containEql([5,1]);
-					data.should.containEql([5,2]);
-					data.should.containEql([5,3]);
+					data.should.containEql([7,3]);
+					data.should.containEql([7,4]);
+					data.should.containEql([7,5]);
 					
 					// top Line
-					data.should.containEql([4,4]);
-					data.should.containEql([3,4]);
-					data.should.containEql([2,4]);
-					data.should.containEql([1,4]);
+					data.should.containEql([6,6]);
+					data.should.containEql([5,6]);
+					data.should.containEql([4,6]);
+					data.should.containEql([3,6]);
 					
 					// left down
-					data.should.containEql([0,3]);
-					data.should.containEql([0,2]);
-					data.should.containEql([0,1]);
+					data.should.containEql([2,5]);
+					data.should.containEql([2,4]);
+					data.should.containEql([2,3]);
 					
 					done();
 				});
@@ -339,47 +340,47 @@ describe('Simple Square-Polygon', function(){
 			it('should return a full filled element', function(done){
 
 				s.render(function(data){		
-					// Points
-					data.should.containEql([0,0]);
-					data.should.containEql([5,0]);
-					data.should.containEql([5,4]);
-					data.should.containEql([0,4]);
+					// points
+					data.should.containEql([2,2]);
+					data.should.containEql([7,2]);
+					data.should.containEql([7,6]);
+					data.should.containEql([2,6]);
 					
 					// bottom Line
-					data.should.containEql([1,0]);
-					data.should.containEql([2,0]);
-					data.should.containEql([3,0]);
-					data.should.containEql([4,0]);
+					data.should.containEql([3,2]);
+					data.should.containEql([4,2]);
+					data.should.containEql([5,2]);
+					data.should.containEql([6,2]);
 					
 					// right up
-					data.should.containEql([5,1]);
-					data.should.containEql([5,2]);
-					data.should.containEql([5,3]);
+					data.should.containEql([7,3]);
+					data.should.containEql([7,4]);
+					data.should.containEql([7,5]);
 					
 					// top Line
-					data.should.containEql([4,4]);
-					data.should.containEql([3,4]);
-					data.should.containEql([2,4]);
-					data.should.containEql([1,4]);
+					data.should.containEql([6,6]);
+					data.should.containEql([5,6]);
+					data.should.containEql([4,6]);
+					data.should.containEql([3,6]);
 					
 					// left down
-					data.should.containEql([0,3]);
-					data.should.containEql([0,2]);
-					data.should.containEql([0,1]);
+					data.should.containEql([2,5]);
+					data.should.containEql([2,4]);
+					data.should.containEql([2,3]);
 					
 					// fill
-					data.should.containEql([1,1]);
-					data.should.containEql([1,2]);
-					data.should.containEql([1,3]);
-					data.should.containEql([2,1]);
-					data.should.containEql([2,2]);
-					data.should.containEql([2,3]);
-					data.should.containEql([3,1]);
-					data.should.containEql([3,2]);
 					data.should.containEql([3,3]);
-					data.should.containEql([4,1]);
-					data.should.containEql([4,2]);
+					data.should.containEql([3,4]);
+					data.should.containEql([2,5]);
 					data.should.containEql([4,3]);
+					data.should.containEql([4,4]);
+					data.should.containEql([4,5]);
+					data.should.containEql([5,3]);
+					data.should.containEql([5,4]);
+					data.should.containEql([5,5]);
+					data.should.containEql([6,3]);
+					data.should.containEql([6,4]);
+					data.should.containEql([6,5]);
 					
 					done();
 				});
